4d79f918dbaf0bd64a446737c3f3e8ccb34c305f
Brentice Bush <btbush@gmail.com>
2017-02-09 14:38:24 -0500

    completed creating dating_site database and migrating users table

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -5,5 +5,4 @@ class User < ActiveRecord::Base
   validates :age numericality { only_integer: true, greater_than: 18 }
   validates :gender, inclusion: { in: ['male', 'female'] }
   validates :image, presence: true
-  has_many :likes
 end

d6802b4081f31f6cb045aebe682300cacf4155b7
Brentice Bush <btbush@gmail.com>
2017-02-10 12:47:58 -0500

    add tests for users api

--- lib/resources/likes_api.rb
+++ lib/resources/likes_api.rb
@@ -5,7 +5,7 @@ require_relative '../models/user'
 database_config = ENV['DATABASE_URL']
 
 if database_config.blank?
-  database_config = YAML::load(File.open('config/database.yml'))
+  database_config = YAML::load(File.open('config/test_database.yml'))
 end
 
 ActiveRecord::Base.establish_connection(database_config)
@@ -18,8 +18,9 @@ post '/api/users/:id/likes/:crush_id' do |id, crush_id|
   user = User.find_by_id(id)
   crush = User.find_by_id(crush_id)
 
-  halt(404, { message:'invalid id'}.to_json) if (user.nil? || crush.nil?)
+  halt(404, { message:'Invalid id'}.to_json) if (user.nil? || crush.nil?)
 
-  Like.create(user_id: id, crush_id: crush_id)
-  status 201
+  like = Like.create(user_id: id, crush_id: crush_id)
+
+  [201, like.to_json]
 end

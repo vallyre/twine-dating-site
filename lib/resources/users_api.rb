fd3d731badd33556a7b1e2e81e0f001b894b5214
Brentice Bush <btbush@gmail.com>
2017-02-12 11:24:18 -0500

    get last user created

--- lib/resources/users_api.rb
+++ lib/resources/users_api.rb
@@ -17,6 +17,7 @@ end
 #Get all users
 #Optional: filter by gender (e.g. /api/users?gender=male)
 #Optional: select number of results (e.g. /api/users?gender=female&results=10, /api/users?results=100, etc.)
+#Optional: get last users id by position (e.g. /api/users?position=last)
 get '/api/users' do
   users = User.all
 
@@ -28,6 +29,11 @@ get '/api/users' do
   results = params[:results].to_i
   if results > 0
     users = users.sample(results)
+  end
+
+  position = params[:position]
+  if position == 'last'
+    users = users.last
   end
 
   users.to_json
